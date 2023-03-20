4457b261915059c99c790424faf6225bcc6a002c
KhetwalDevesh <deveshkhetwal05@gmail.com>
2023-03-20 12:25:55 +0530

    ecom-without-auth init

---
+++ src/components/Products.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Products = () => {
+	return (
+		<div className="px-[5rem]">
+			<div>
+				<img
+					src="https://cdn.shopify.com/s/files/1/0656/2424/4438/products/IMG-7701.jpg?v=1663070639&width=1100"
+					className="dirt-hat w-[25rem] h-[25rem] rounded-2xl"
+				/>
+				<div></div>
+			</div>
+		</div>
+	);
+};
+
+export default Products;
